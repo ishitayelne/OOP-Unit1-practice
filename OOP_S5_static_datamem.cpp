8021e535b3d42e3aa5c52a27d357c1b78c1b9020
Ishita Yelne <ishitayelne27@gmail.com>
2026-08-22 21:38:43 +0530

    Create OOP_S5_static_datamem.cpp

---
+++ OOP_S5_static_datamem.cpp
@@ -0,0 +1,43 @@
+//Static data member & member function
+//
+#include <iostream>
+using namespace std;
+class material
+{
+  int id;
+  static int count; // Declaration of a static data member
+
+  public:
+    void getdata(int a)
+    {
+      id=a;
+      count++; // Increment count
+    }
+    void display()
+    {
+      cout << "\nCount: " << count ;
+    }
+};
+
+// Definition and initialization of the static data member outside the class
+int material :: count;
+
+int main()
+{
+  material A,B,C;
+  
+  A.display();
+  B.display();
+  C.display();
+    
+  A.getdata(100);
+  B.getdata(200);
+  C.getdata(300);
+  
+  cout<<"\n\nAfter reading values: ";
+  
+  A.display();
+  B.display();
+  C.display();
+  return 0;
+}
