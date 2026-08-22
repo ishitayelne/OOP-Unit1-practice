568f99bea184ed8fc6c9a6f2c9ae38b0404783c2
Ishita Yelne <ishitayelne27@gmail.com>
2026-08-22 22:05:46 +0530

    Update OOP_S5_static_memfn.cpp

--- OOP_S5_static_memfn.cpp
+++ OOP_S5_static_memfn.cpp
@@ -1,1 +1,30 @@
 //Static member function
+//Write a program to calculate the number of objects created
+#include <iostream>
+using namespace std;
+
+class Test
+{
+    static int count;
+
+  public:
+    Test() { count++; } // Increments count automatically when obj is created
+
+    static void showCount() {
+        cout << "Total objects: " << count << endl;
+    }
+};
+
+int Test::count = 0;
+
+int main() {
+    int n;
+    cout << "Enter number of objects: ";
+    cin >> n;
+
+    Test obj[n]; // Creates 'n' obj
+
+    Test::showCount();
+
+    return 0;
+}
